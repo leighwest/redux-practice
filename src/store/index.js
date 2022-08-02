333d6290874c23d81ccb78792408af3f0457c6b2
Leigh West <leighwest@hotmail.com>
2022-08-02 22:44:26 +0000

    Provided the store, added .prettierrc.json

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,14 @@
-import { createStore } from "redux";
+import { createStore } from 'redux';
 
 // reducer will run when component loads so you need to give it a default (initial) state
 const counterReducer = (state = { counter: 0 }, action) => {
-  if (action.type === "increment") {
+  if (action.type === 'increment') {
     return {
       counter: state.counter + 1,
     };
   }
 
-  if (action.type === "decrement") {
+  if (action.type === 'decrement') {
     return {
       counter: state.counter - 1,
     };
