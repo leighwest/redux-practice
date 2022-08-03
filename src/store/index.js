8b442326e2fdcbfefd7a562881588964a0113ff2
Leigh West <leighwest@hotmail.com>
2022-08-03 09:23:31 +0000

    Added ability to toggle visibility of counter

--- src/store/index.js
+++ src/store/index.js
@@ -1,22 +1,36 @@
 import { createStore } from 'redux';
 
 // reducer will run when component loads so you need to give it a default (initial) state
-const counterReducer = (state = { counter: 0 }, action) => {
+const initialState = { counter: 0, showCounter: true };
+
+const counterReducer = (state = initialState, action) => {
   if (action.type === 'increment') {
     return {
       counter: state.counter + 1,
+      // even though we're not changing this property, we have to return it because redux won't merge
+      // changed states existing states (we have to specify it whether it changes or not)
+      showCounter: state.showCounter,
     };
   }
 
   if (action.type === 'increase') {
     return {
       counter: state.counter + action.amount,
+      showCounter: state.showCounter,
     };
   }
 
   if (action.type === 'decrement') {
     return {
       counter: state.counter - 1,
+      showCounter: state.showCounter,
+    };
+  }
+
+  if (action.type === 'toggle') {
+    return {
+      showCounter: !state.showCounter,
+      counter: state.counter,
     };
   }
 
