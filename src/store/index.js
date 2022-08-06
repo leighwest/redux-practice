d2753ae68e890801f5d7270fd29a958adce118d4
Leigh West <leighwest@hotmail.com>
2022-08-06 01:14:59 +0000

    Added redux library slice

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,29 @@
 import { createStore } from 'redux';
+import { createSlice } from '@reduxjs/toolkit';
 
 // reducer will run when component loads so you need to give it a default (initial) state
 const initialState = { counter: 0, showCounter: true };
+
+createSlice({
+  name: 'counter',
+  initialState, // JS will interpret this as initialState: initialState (i.e. line 5 variable)
+  reducers: {
+    // map of reducers this slice needs
+    increment(state) {
+      state.counter++; // although it looks like we're mutating state, redux library will detect it and will clone state, edit
+      // what we're editing, and add what we're not in an immutable way
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.amount;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
 
 const counterReducer = (state = initialState, action) => {
   if (action.type === 'increment') {
